355a9760ed5a53965627c4543b2d269c469c03c0
ALFONSO EDUARDO MARQUEZ CHAMORRO <amarquez6@us.es>
2026-03-12 10:23:31 +0100

    Update User.java

--- src/main/java/aiss/videominer/model/User.java
+++ src/main/java/aiss/videominer/model/User.java
@@ -10,12 +10,6 @@ import jakarta.persistence.*;
 @Table(name = "VMUser")
 public class User {
 
-    /*
-    * In order to avoid making the model unnecessarily complex, we establish a one-to-one relationship between Comment and
-    * User (instead of many-to-one). This causes an exception if we try to add a Comment to the DataBase that has been
-    * created by a User that already has a Comment in a previously stored Video. To avoid this exception, we automatically
-    * assign an id to each new User with AutoIncrement.
-     */
     @Id
     @JsonProperty("id")
     @GeneratedValue(strategy = GenerationType.AUTO)
